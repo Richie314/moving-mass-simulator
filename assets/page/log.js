4778ae6056e6e3d9f3b187db4a36a27b2f1f829c
Riccardo Ciucci <richieinleghorn@gmail.com>
2023-03-23 17:50:17 +0100

    Aggiunte varie

--- assets/page/log.js
+++ assets/page/log.js
@@ -2,13 +2,14 @@ const log_div = document.getElementById('logs');
 if (!log_div) {
     throw new Error('div#logs not found!');
 }
-const log_data = (message, classname) =>
+const logData = (message, classname = '') =>
 {
-    if (message == '' || classname == '')
+    if (message == '')
         return;
     const messageDiv = document.createElement('pre');
     messageDiv.classList.add('log');
-    messageDiv.classList.add(classname);
+    if (classname != '')
+        messageDiv.classList.add(classname);
     messageDiv.innerHTML = message;
     log_div.appendChild(messageDiv);
     messageDiv.scrollIntoView({
@@ -17,16 +18,61 @@ const log_data = (message, classname) =>
         block: 'end'
     });
 }
+const dotsToStr = function( ...args)
+{ 
+    if (!args || args.length === 0)
+    {
+        return '';
+    }
+    const first = args.shift();
+    const others = args;
+    if (first == null)
+    {
+        return 'null ' + dotsToStr(...others);
+    }
+    if (first == undefined)
+    {
+        return 'undefined ' + dotsToStr(...others);
+    }
+    if (typeof first === 'string' || typeof first === 'number' || typeof first === 'bigint')
+    {
+        return first + ' ' + dotsToStr(...others);
+    }
+    if (typeof first === 'boolean')
+    {
+        return (first ? 'true' : 'false') + ' ' + dotsToStr(...others);
+    }
+    if ('toString' in first.__proto__)
+    {
+        return first.toString() + ' ' + dotsToStr(...others);
+    }
+    return '<unknown> ' + dotsToStr(...others);
+}
 const log = function(...message)
 {
+    if (!message)
+    {
+        return;
+    }
     console.log(...message);
-
+    logData(dotsToStr(...message), '');
 }
 const warn = function(...message)
 {
+    if (!message)
+    {
+        return;
+    }
     console.warn(...message);
+    logData(dotsToStr(...message), 'warn');
 }
 const err = function(...message)
 {
+    if (!message)
+    {
+        return;
+    }
     console.error(...message);
+    logData(dotsToStr(...message), 'error');
 }
+document.addEventListener('error', evt => err(evt.message));
