7ddb8712798df22573b6d0e4e275639ac2a5bf38
Richie314 <richieinleghorn@gmail.com>
2023-03-31 15:22:48 +0200

    Ui additions

--- assets/page/ui.js
+++ assets/page/ui.js
@@ -1,8 +1,16 @@
 const promptMathBtn = document.getElementById('show-math-btn');
 const promptShellBtn = document.getElementById('show-terminal-btn');
+const promptSettingsBtn = document.getElementById('show-settings');
 promptMathBtn.onclick = () => {
     document.getElementById('math').classList.add('show');
 }
 promptShellBtn.onclick = () => {
     document.getElementById('shell').classList.add('show');
+}
+promptSettingsBtn.onclick = () => {
+    simulation.pause();
+    document.getElementById('settings').classList.add('show');
+}
+document.getElementById('close-settings').onclick = () => {
+    document.getElementById('settings').classList.remove('show');
 }
